9db5e4cc9480a144eeb69dcc2a381a790db784cb
seunggabi <seunggabi@naver.com>
2022-11-28 17:24:42 +0900

    feat: trivial resolver & query & mutation

--- index.js
+++ index.js
@@ -2,12 +2,46 @@ const {ApolloServer} = require("apollo-server")
 
 const typeDefs = `
   type Query {
-  totalPhotos: Int!
-}
+    totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
+  }
+  
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
 `
+
+let _id = 0
+let photos = []
+
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+
+  Mutation: {
+    postPhoto(parent, args) {
+      const photo = {
+        id: _id++,
+        ...args
+      }
+
+      photos.push(photo)
+
+      return photo
+    }
+  },
+
+  Photo: {
+    url: parent => `http://yoursite.com/img/${parent.id}.png`
   }
 }
 
