b27aaf4d2df56d03b6cd2ac1ad7686e5bd042a1e
seunggabi <seunggabi@naver.com>
2022-11-29 00:57:35 +0900

    feat: photo by github user

--- index.js
+++ index.js
@@ -60,93 +60,55 @@ const typeDefs = `
   }
 `;
 
-let _id = 0;
-const users = [
-  {
-    githubLogin: "mHattrup",
-    name: "Mike Hattrup"
-  },
-  {
-    githubLogin: "gPlake",
-    name: "Glen Plake"
-  },
-  {
-    githubLogin: "sSchmidt",
-    name: "Scot Schmidt"
-  },
-];
-const photos = [
-  {
-    id: "1",
-    name: "Dropping the Heart Chute",
-    description: "The heart chute is one of my favorite chutes",
-    category: "ACTION",
-    githubUser: "gPlake",
-    created: "3-28-1977"
-  },
-  {
-    id: "2",
-    name: "Enjoying the sunshine",
-    category: "SELFIE",
-    githubUser: "sSchmidt",
-    created: "1-2-1985"
-  },
-  {
-    id: "3",
-    name: "Gunbarrel 25",
-    deescription: "25 laps on gunbarrel today",
-    category: "LANDSCAPE",
-    githubUser: "sSchmidt",
-    created: "2018-04-15T19:07:57.308Z"
-  },
-];
-const tags = [
-  {
-    photoID: "1",
-    userID: "gPlake"
-  },
-  {
-    photoID: "2",
-    userID: "sSchmidt"
-  },
-  {
-    photoID: "2",
-    userID: "mHattrup"
-  },
-  {
-    photoID: "2",
-    userID: "gPlake"
-  },
-];
+const users = [{
+  githubLogin: "mHattrup", name: "Mike Hattrup"
+}, {
+  githubLogin: "gPlake", name: "Glen Plake"
+}, {
+  githubLogin: "sSchmidt", name: "Scot Schmidt"
+},];
+const photos = [{
+  id: "1",
+  name: "Dropping the Heart Chute",
+  description: "The heart chute is one of my favorite chutes",
+  category: "ACTION",
+  githubUser: "gPlake",
+  created: "3-28-1977"
+}, {
+  id: "2", name: "Enjoying the sunshine", category: "SELFIE", githubUser: "sSchmidt", created: "1-2-1985"
+}, {
+  id: "3",
+  name: "Gunbarrel 25",
+  deescription: "25 laps on gunbarrel today",
+  category: "LANDSCAPE",
+  githubUser: "sSchmidt",
+  created: "2018-04-15T19:07:57.308Z"
+},];
 
 const resolvers = {
   Query: {
-    totalPhotos: (parent, args, {db}) =>
-      db.collection("photos")
-        .estimatedDocumentCount(),
-    allPhotos: (parent, args, {db}) =>
-      db.collection("photos")
-        .find()
-        .toArray(),
-    totalUsers: (parent, args, {db}) =>
-      db.collections("users")
-        .estimatedDocumentCount(),
-    allUsers: (parent, args, {db}) =>
-      db.collections("users")
-        .find()
-        .toArray(),
-    me: (parent, args, {currentUser}) => currentUser
+    totalPhotos: (parent, args, {db}) => db.collection("photos")
+      .estimatedDocumentCount(), allPhotos: (parent, args, {db}) => db.collection("photos")
+      .find()
+      .toArray(), totalUsers: (parent, args, {db}) => db.collections("users")
+      .estimatedDocumentCount(), allUsers: (parent, args, {db}) => db.collections("users")
+      .find()
+      .toArray(), me: (parent, args, {currentUser}) => currentUser
   },
 
   Mutation: {
-    postPhoto(parent, args) {
+    async postPhoto(parent, args, {db, currentUser}) {
+      if (!currentUser) {
+        throw new Error("only an authorized user can post a photo");
+      }
+
       const photo = {
-        id: _id++,
-        ...args.input,
-        created: new Date()
+        ...args.input, userID: currentUser.githubLogin, created: new Date()
       };
 
-      photos.push(photo);
+      const {insertedIds} = await db.collection("photos")
+        .insert(photo);
+      photo.id = insertedIds[0]
 
       return photo;
     },
@@ -154,15 +116,9 @@ const resolvers = {
     // https://github.com/login/oauth/authorize?client_id=Iv1.73e2a9dfda956446&scope=seunggabi
     async githubAuth(parent, {code}, {db}) {
       let {
-        message,
-        access_token,
-        avatar_url,
-        login,
-        name
+        message, access_token, avatar_url, login, name
       } = await authorizeWithGithub({
-        client_id: "Iv1.73e2a9dfda956446",
-        client_secret: "",
-        code
+        client_id: "Iv1.73e2a9dfda956446", client_secret: "", code
       })
 
       if (message) {
@@ -170,10 +126,7 @@ const resolvers = {
       }
 
       let latestUserInfo = {
-        name,
-        githubLogin: login,
-        githubToken: access_token,
-        avatar: avatar_url
+        name, githubLogin: login, githubToken: access_token, avatar: avatar_url
       }
 
       await db
@@ -185,28 +138,20 @@ const resolvers = {
   },
 
   Photo: {
-    url: (parent) => `http://yoursite.com/img/${parent.id}.png`,
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser)
+    url: parent => `/img/photos/${parent._id}.jpg`,
+    postedBy: (parent, args, {db}) => {
+      return db.collection("users").findOne({githubLogin: parent.userID})
     },
-    taggedUsers: parent => tags
-      .filter(tag => tag.photoID === parent.id)
-      .map(tag => tag.userID)
-      .map(userID => users.find(u => u.githubLogin === userID))
+    id: parent => parent.id || parent._id
   },
 
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
     },
-    inPhotos: parent => tags
-      .filter(tag => tag.userID === parent.id)
-      .map(tag => tag.userID)
-      .map(photoID => photos.find(p => p.id === photoID))
   },
 
-  DateTime: new GraphQLScalarType
-  ({
+  DateTime: new GraphQLScalarType({
     name: "DateTime",
     description: "A valid date time value.",
     parseValue: value => new Date(value),
@@ -215,31 +160,25 @@ const resolvers = {
   })
 };
 
-const requestGithubToken = credentials =>
-  fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(credentials)
-  })
-    .then(res => res.json())
-    .catch(error => {
-      throw new Error(JSON.stringify(error));
-    });
+const requestGithubToken = credentials => fetch("https://github.com/login/oauth/access_token", {
+  method: "POST", headers: {
+    "Content-Type": "application/json", "Accept": "application/json"
+  }, body: JSON.stringify(credentials)
+})
+  .then(res => res.json())
+  .catch(error => {
+    throw new Error(JSON.stringify(error));
+  });
 
-const requestGithubUserAccount = token =>
-  fetch(`https://api.github.com/user`, {
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `token ${token}`
-    },
-  })
-    .then(res => res.json())
-    .catch(error => {
-      throw new Error(JSON.stringify(error));
-    });
+const requestGithubUserAccount = token => fetch(`https://api.github.com/user`, {
+  headers: {
+    "Content-Type": "application/json", "Authorization": `token ${token}`
+  },
+})
+  .then(res => res.json())
+  .catch(error => {
+    throw new Error(JSON.stringify(error));
+  });
 
 const authorizeWithGithub = async (credentials) => {
   const {access_token} = await requestGithubToken(credentials);
@@ -252,12 +191,9 @@ const start = async () => {
   const DB = process.env.DB_HOST;
   const PORT = 4000;
 
-  const client = await MongoClient.connect(
-    DB,
-    {
-      useNewUrlParser: true
-    }
-  );
+  const client = await MongoClient.connect(DB, {
+    useNewUrlParser: true
+  });
   const db = client.db();
 
   const server = new ApolloServer({
@@ -282,9 +218,7 @@ const start = async () => {
     .get("/playground", expressPlayground({endpoint: "/graphql"}))
 
   app.listen(PORT, () => {
-    console.log(
-      `GraphQL Server running @ http://localhost:${PORT}${server.graphqlPath}`
-    );
+    console.log(`GraphQL Server running @ http://localhost:${PORT}${server.graphqlPath}`);
   });
 };
 
