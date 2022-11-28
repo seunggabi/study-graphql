f592c8d76d75902ac0ba4dc4d0b863a668f90ea5
seunggabi <seunggabi@naver.com>
2022-11-28 18:04:14 +0900

    feat: tags

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
   
   type Query {
@@ -32,6 +33,7 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
   
   input PostPhotoInput {
@@ -43,9 +45,18 @@ const typeDefs = `
 
 let _id = 0;
 const users = [
-  {githubLogin: 'mHattrup', name: 'Mike Hattrup'},
-  {githubLogin: 'gPlake', name: 'Glen Plake'},
-  {githubLogin: 'sSchmidt', name: 'Scot Schmidt'},
+  {
+    githubLogin: 'mHattrup',
+    name: 'Mike Hattrup'
+  },
+  {
+    githubLogin: 'gPlake',
+    name: 'Glen Plake'
+  },
+  {
+    githubLogin: 'sSchmidt',
+    name: 'Scot Schmidt'
+  },
 ];
 const photos = [
   {
@@ -67,6 +78,24 @@ const photos = [
     deescription: '25 laps on gunbarrel today',
     category: 'LANDSCAPE',
     githubUser: 'sSchmidt',
+  },
+];
+const tags = [
+  {
+    photoID: "1",
+    userID: "gPlake"
+  },
+  {
+    photoID: "2",
+    userID: "sSchmidt"
+  },
+  {
+    photoID: "2",
+    userID: "mHattrup"
+  },
+  {
+    photoID: "2",
+    userID: "gPlake"
   },
 ];
 
@@ -91,7 +120,24 @@ const resolvers = {
 
   Photo: {
     url: (parent) => `http://yoursite.com/img/${parent.id}.png`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser)
+    },
+    taggedUsers: parent => tags
+      .filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
   },
+
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.userID)
+      .map(photoID => photos.find(p => p.id === photoID))
+  }
 };
 
 const server = new ApolloServer({
