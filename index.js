699129ae3e391e1263cb277f044e5c6d0c1589df
seunggabi <seunggabi@naver.com>
2022-11-28 18:28:21 +0900

    feat: add created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const {ApolloServer} = require('apollo-server');
+const {ApolloServer} = require("apollo-server");
+const {GraphQLScalarType} = require("graphql/type");
 
 const typeDefs = `
+  scalar DateTime
+  
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -34,6 +37,7 @@ const typeDefs = `
     category: PhotoCategory!
     postedBy: User!
     taggedUsers: [User!]!
+    created: DateTime!
   }
   
   input PostPhotoInput {
@@ -46,38 +50,41 @@ const typeDefs = `
 let _id = 0;
 const users = [
   {
-    githubLogin: 'mHattrup',
-    name: 'Mike Hattrup'
+    githubLogin: "mHattrup",
+    name: "Mike Hattrup"
   },
   {
-    githubLogin: 'gPlake',
-    name: 'Glen Plake'
+    githubLogin: "gPlake",
+    name: "Glen Plake"
   },
   {
-    githubLogin: 'sSchmidt',
-    name: 'Scot Schmidt'
+    githubLogin: "sSchmidt",
+    name: "Scot Schmidt"
   },
 ];
 const photos = [
   {
-    id: '1',
-    name: 'Dropping the Heart Chute',
-    description: 'The heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+    created: "3-28-1977"
   },
   {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+    created: "1-2-1985"
   },
   {
-    id: '3',
-    name: 'Gunbarrel 25',
-    deescription: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
+    id: "3",
+    name: "Gunbarrel 25",
+    deescription: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+    created: "2018-04-15T19:07:57.308Z"
   },
 ];
 const tags = [
@@ -110,6 +117,7 @@ const resolvers = {
       const photo = {
         id: _id++,
         ...args.input,
+        created: new Date()
       };
 
       photos.push(photo);
@@ -137,7 +145,15 @@ const resolvers = {
       .filter(tag => tag.userID === parent.id)
       .map(tag => tag.userID)
       .map(photoID => photos.find(p => p.id === photoID))
-  }
+  },
+
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value.",
+    parseValue: value => new Date(vlaue),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 };
 
 const server = new ApolloServer({
